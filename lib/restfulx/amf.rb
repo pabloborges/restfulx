87d18e51060f882f2e49a412044f087d9e0a89b4
Pablo <pablo@axon.com.br>
2010-12-15 11:34:42 -0300

    removing warning at initialization

--- lib/restfulx/amf.rb
+++ lib/restfulx/amf.rb
@@ -4,17 +4,17 @@ $:.unshift "#{File.expand_path(File.dirname(__FILE__))}/amf/"
 require 'amf/class_mapping'
 
 module RestfulX::AMF
-  
+
   # begin
   #   require 'amf/ext'
   #   puts "using native C AMF serializer"
   # rescue LoadError
     require 'amf/pure'
-    puts "using pure AMF serializer"
+  #   puts "using pure AMF serializer"
   # end
-    
+
   ClassMapper = RestfulX::AMF::ClassMapping.new
-  
+
   # The base exception for AMF errors.
   class AMFError < StandardError; end
 end
